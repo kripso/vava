2c3fdd9c376240f2faf8fed264f7d7cb9cc5bdab
kripso <kripsoworld@gmail.com>
2021-02-18 19:37:09 +0100

    first lecture

---
+++ test/src/first_lecture/Main.java
@@ -0,0 +1,10 @@
+package test.src.first_lecture;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("4");
+
+    }
+}
---
+++ test/src/first_lecture/Televizor.java
@@ -0,0 +1,19 @@
+package test.src.first_lecture;
+
+public class Televizor {
+
+    private String znacka;
+    private double price;
+    private float uhlopriecka;
+    private boolean isOn;
+    private int weight;
+
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("hh");
+        Televizor tvSonyKDLx56 = new Televizor();
+        Televizor newtv=new Televizor();
+    }
+
+}
